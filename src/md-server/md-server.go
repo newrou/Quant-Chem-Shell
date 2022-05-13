f0a8f27f24db8f2910535c7db6e47cce96a46c82
alex <alex@debian-BULLSEYE-live-builder-AMD64>
2022-05-13 13:02:57 +0700

    add view of work

--- src/md-server/md-server.go
+++ src/md-server/md-server.go
@@ -33,14 +33,14 @@ type PageData struct {
     WorkList     []Work
 }
 
-func LoadWork(fname, id string) Work {
+func LoadWork(id string) Work {
     var w Work
+    fname := fmt.Sprintf("%s/%s", works_dir, id)
     content, err := ioutil.ReadFile(fname)
     if err != nil { log.Fatal("Error when opening file: ", err) }
     var data map[string]interface{}
     err = json.Unmarshal(content, &data)
     if err != nil { log.Fatal("Error during Unmarshal(): ", err) }
-//    fmt.Println(data)
 //    fmt.Println(reflect.TypeOf(data["Title"]), data["Title"])
     w.Id = id
     w.Title = fmt.Sprintf("%s", data["Title"])
@@ -57,13 +57,8 @@ func GetWorkList(works []Work) []Work {
     files, err := ioutil.ReadDir(works_dir)
     if err != nil { log.Fatal(err) }
     for _, file := range files {
-//        fmt.Println(file.Name(), file.IsDir())
-	fname := fmt.Sprintf("%s/%s", works_dir, file.Name())
-//        fmt.Println(fname)
-	w := LoadWork(fname, file.Name())
-//        fmt.Println(w, w.Status)
+	w := LoadWork(file.Name())
 	works = append(works, w)
-//	works = append(works, Work{Title: fname, Status: "done", Done: false})
     }
     return works
 }
@@ -79,6 +74,20 @@ func main() {
 	tmpl_list := template.Must(template.ParseFiles("form_list.html"))
 	data.WorkList = GetWorkList(data.WorkList)
         tmpl_list.Execute(w, data)
+	fmt.Println("View list of works")
+    })
+
+
+    http.HandleFunc("/view", func(w http.ResponseWriter, r *http.Request) {
+	tmpl_view := template.Must(template.ParseFiles("form_view.html"))
+        if r.Method != http.MethodGet {
+            tmpl_view.Execute(w, nil)
+            return
+        }
+	Id := r.FormValue("id")
+	data := LoadWork(Id)
+        tmpl_view.Execute(w, data)
+	fmt.Println("View work: ", Id)
     })
 
 
@@ -93,10 +102,10 @@ func main() {
 	p2 := fmt.Sprintf("%s/%s", archiv_dir, Id)
 	e := os.Rename(p1, p2)
 	if e != nil { /* log.Fatal(e) */ }
-	msg := fmt.Sprintf("Remove works: %s", Id)
-	fmt.Println(msg)
+	msg := fmt.Sprintf("Remove work: %s", Id)
         data := PageData { Message: msg, }
         tmpl_auto.Execute(w, data)
+	fmt.Println("Remove work: ", Id)
     })
 
 
@@ -106,7 +115,6 @@ func main() {
             tmpl_add.Execute(w, nil)
             return
         }
-
         work := Work{
             Title:     r.FormValue("Title"),
             Temp:      r.FormValue("Temp"),
@@ -115,7 +123,6 @@ func main() {
             Compounds: r.FormValue("Compounds"),
             Status:    r.FormValue("Status"),
         }
-
         dat, err := json.MarshalIndent(work, "", " ")
         if err != nil { fmt.Println(err) }
         utime := int32(time.Now().Unix())
@@ -125,11 +132,13 @@ func main() {
         if err != nil { fmt.Println(err) }
 	fname := fmt.Sprintf("%s/%s", works_dir, work.Id)
         _ = ioutil.WriteFile(fname, dat, 0644)
-        fmt.Println(fname, string(dat))
+//        fmt.Println(fname, string(dat))
         _ = work
         tmpl_add.Execute(w, struct{ Success bool }{true})
+	fmt.Println("Add new work: ", work.Id)
     })
 
 
     http.ListenAndServe(":8080", nil)
 }
+
